13f1bfca61890bb3574a637d28f9cd50e429f5eb
Sara <sara.aspery@alfresco.com>
2024-10-02 08:24:03 +0100

    Revert "ACS-6284 Add audit to RepoEvent (#32)" (#36)
    This reverts commit 81e55dc92d07e4897a5a426adc419c895168d594.

--- src/main/java/org/alfresco/repo/event/v1/model/EventType.java
+++ src/main/java/org/alfresco/repo/event/v1/model/EventType.java
@@ -2,7 +2,7 @@
  * #%L
  * Alfresco Repository
  * %%
- * Copyright (C) 2005 - 2024 Alfresco Software Limited
+ * Copyright (C) 2005 - 2020 Alfresco Software Limited
  * %%
  * This file is part of the Alfresco software.
  * If the software was purchased under a paid Alfresco license, the terms of
@@ -35,8 +35,7 @@ public enum EventType
     NODE_CREATED(EventTypeConst.CREATED, ContextType.NODE), NODE_UPDATED(EventTypeConst.UPDATED, ContextType.NODE), NODE_DELETED(EventTypeConst.DELETED, ContextType.NODE),
     CHILD_ASSOC_CREATED(EventTypeConst.CREATED, ContextType.CHILD_ASSOC), CHILD_ASSOC_DELETED(EventTypeConst.DELETED, ContextType.CHILD_ASSOC),
     PEER_ASSOC_CREATED(EventTypeConst.CREATED, ContextType.PEER_ASSOC), PEER_ASSOC_DELETED(EventTypeConst.DELETED, ContextType.PEER_ASSOC),
-    PERMISSION_UPDATED(EventTypeConst.UPDATED, ContextType.PERMISSION),
-    AUDIT_ENTRY_CREATED(EventTypeConst.CREATED, ContextType.AUDIT);
+    PERMISSION_UPDATED(EventTypeConst.UPDATED, ContextType.PERMISSION);
 
     private static final String PREFIX = "org.alfresco.event.";
     private final String type;
@@ -71,7 +70,7 @@ public enum EventType
 
     private enum ContextType
     {
-        NODE("node."), CHILD_ASSOC("assoc.child."), PEER_ASSOC("assoc.peer."), PERMISSION("permission."), AUDIT("audit.");
+        NODE("node."), CHILD_ASSOC("assoc.child."), PEER_ASSOC("assoc.peer."), PERMISSION("permission.");
 
         private final String context;
         ContextType(String context)
