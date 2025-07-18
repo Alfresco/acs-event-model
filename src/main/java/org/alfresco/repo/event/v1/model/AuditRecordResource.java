8cced91a01850da28656f80b3f7a23de67472ecc
vivekkr0311 <vivek.kumar@hyland.com>
2025-07-18 16:10:53 +0530

    [ACS-9885]: Basic code changes

--- src/main/java/org/alfresco/repo/event/v1/model/AuditRecordResource.java
+++ src/main/java/org/alfresco/repo/event/v1/model/AuditRecordResource.java
@@ -2,4 +2,37 @@ package org.alfresco.repo.event.v1.model;
 
 public abstract class AuditRecordResource implements Resource
 {
+    private String auditApplicationId;
+    private String auditData;
+
+    public AuditRecordResource()
+    {
+
+    }
+
+    public AuditRecordResource(String auditApplicationId, String auditData)
+    {
+        this.auditApplicationId = auditApplicationId;
+        this.auditData = auditData;
+    }
+
+    public String getAuditApplicationId()
+    {
+        return auditApplicationId;
+    }
+
+    public void setAuditApplicationId(String auditApplicationId)
+    {
+        this.auditApplicationId = auditApplicationId;
+    }
+
+    public String getAuditData()
+    {
+        return auditData;
+    }
+
+    public void setAuditData(String auditData)
+    {
+        this.auditData = auditData;
+    }
 }
