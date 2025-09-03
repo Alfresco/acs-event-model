e12921e5cbb16560891a2409b6407b85f1fdd049
Kacper Magdziarz <kacper.magdziarz@hyland.com>
2025-09-03 15:55:18 +0200

    [ACS-9722] Fix ordering

--- src/main/java/org/alfresco/repo/event/v1/model/AuditEntryResource.java
+++ src/main/java/org/alfresco/repo/event/v1/model/AuditEntryResource.java
@@ -7,8 +7,8 @@ import java.util.Objects;
 public class AuditEntryResource implements Resource
 {
     private String entryType;
-    private Long entryDBId;
     private String username;
+    private Long entryDBId;
     private Map<String, Serializable> entryData;
 
     public AuditEntryResource()
@@ -17,14 +17,9 @@ public class AuditEntryResource implements Resource
     public AuditEntryResource(String entryType, Map<String, Serializable> entryData, String username, Long entryDBId)
     {
         this.entryType = entryType;
-        this.entryData = entryData;
         this.username = username;
         this.entryDBId = entryDBId;
-    }
-
-    public Map<String, Serializable> getEntryData()
-    {
-        return entryData;
+        this.entryData = entryData;
     }
 
     public String getEntryType()
@@ -42,6 +37,11 @@ public class AuditEntryResource implements Resource
         return entryDBId;
     }
 
+    public Map<String, Serializable> getEntryData()
+    {
+        return entryData;
+    }
+
     @Override
     public boolean equals(Object o)
     {
@@ -49,15 +49,15 @@ public class AuditEntryResource implements Resource
             return false;
 
         AuditEntryResource that = (AuditEntryResource) o;
-        return Objects.equals(entryType, that.entryType) && Objects.equals(entryDBId, that.entryDBId) && Objects.equals(username, that.username) && Objects.equals(entryData, that.entryData);
+        return Objects.equals(entryType, that.entryType) && Objects.equals(username, that.username) && Objects.equals(entryDBId, that.entryDBId) && Objects.equals(entryData, that.entryData);
     }
 
     @Override
     public int hashCode()
     {
         int result = Objects.hashCode(entryType);
-        result = 31 * result + Objects.hashCode(entryDBId);
         result = 31 * result + Objects.hashCode(username);
+        result = 31 * result + Objects.hashCode(entryDBId);
         result = 31 * result + Objects.hashCode(entryData);
         return result;
     }
