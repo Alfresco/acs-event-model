c740726652cf21a73a5b78d95e02d2e8c1cb9b3a
Kacper Magdziarz <95610011+kmagdziarz@users.noreply.github.com>
2025-09-03 16:04:18 +0200

    [ACS-9722] Add new fields 'username' and 'entryDBId' to AuditEntryResource (#81)

--- src/main/java/org/alfresco/repo/event/v1/model/AuditEntryResource.java
+++ src/main/java/org/alfresco/repo/event/v1/model/AuditEntryResource.java
@@ -2,29 +2,44 @@ package org.alfresco.repo.event.v1.model;
 
 import java.io.Serializable;
 import java.util.Map;
+import java.util.Objects;
 
 public class AuditEntryResource implements Resource
 {
     private String entryType;
+    private String username;
+    private Long entryDBId;
     private Map<String, Serializable> entryData;
 
     public AuditEntryResource()
     {}
 
-    public AuditEntryResource(String entryType, Map<String, Serializable> entryData)
+    public AuditEntryResource(String entryType, String username, Long entryDBId, Map<String, Serializable> entryData)
     {
         this.entryType = entryType;
+        this.username = username;
+        this.entryDBId = entryDBId;
         this.entryData = entryData;
-    }
-
-    public Map<String, Serializable> getEntryData()
-    {
-        return entryData;
     }
 
     public String getEntryType()
     {
         return entryType;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public Long getEntryDBId()
+    {
+        return entryDBId;
+    }
+
+    public Map<String, Serializable> getEntryData()
+    {
+        return entryData;
     }
 
     @Override
@@ -34,14 +49,16 @@ public class AuditEntryResource implements Resource
             return false;
 
         AuditEntryResource that = (AuditEntryResource) o;
-        return entryType.equals(that.entryType) && entryData.equals(that.entryData);
+        return Objects.equals(entryType, that.entryType) && Objects.equals(username, that.username) && Objects.equals(entryDBId, that.entryDBId) && Objects.equals(entryData, that.entryData);
     }
 
     @Override
     public int hashCode()
     {
-        int result = entryType.hashCode();
-        result = 31 * result + entryData.hashCode();
+        int result = Objects.hashCode(entryType);
+        result = 31 * result + Objects.hashCode(username);
+        result = 31 * result + Objects.hashCode(entryDBId);
+        result = 31 * result + Objects.hashCode(entryData);
         return result;
     }
 }
